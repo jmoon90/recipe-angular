dd482ee975d6221914c224baee6d1012ac96c8af
John Moon <john.moon@livingsocial.com>
2015-10-05 23:27:26 -0400

    angular and bootstrap

--- Gemfile
+++ Gemfile
@@ -51,3 +51,4 @@ end
 
 #angular management
 gem 'bower-rails'
+gem 'angular-rails-templates'
---
+++ app/assets/javascripts/angular_dependencies.js
@@ -0,0 +1,6 @@
+//= require angular/angular
+//= require angular-route/angular-route
+//= require angular-rails-templates
+//= require angular-aria/angular-aria
+//= require angular-animate/angular-animate
+
